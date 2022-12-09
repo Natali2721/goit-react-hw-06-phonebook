0611bbac7bc7f8361e64ad5b4e5888cfacca79a5
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-12-09 16:29:41 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,23 +1,27 @@
-import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { filterContact } from 'redux/store';
 import { Input, P } from 'components/Style/Element.styled';
 
-export const Filter = ({ title, onChange, value }) => (
-  <div>
-    <P>{title}</P>
-    <Input
-      type="text"
-      name="filter"
-      pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-      title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-      onChange={onChange}
-      value={value}
-      placeholder="Enter name"
-    />
-  </div>
-);
+export const Filter = () => {
+  const filter = useSelector(state => state.filter);
+  const dispatch = useDispatch();
 
-Filter.propTypes = {
-  title: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  value: PropTypes.string.isRequired,
+  const onChange = event => {
+    dispatch(filterContact(event.currentTarget.value));
+  };
+
+  return (
+    <div>
+      <P>Find contacts by name</P>
+      <Input
+        type="text"
+        name="filter"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        onChange={onChange}
+        value={filter}
+        placeholder="Enter name"
+      />
+    </div>
+  );
 };
