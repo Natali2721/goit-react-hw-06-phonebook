05dacf36099074533098e64f52739d5807a6cde2
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-12-09 17:15:24 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,19 @@
-import { useEffect } from 'react';
-//import { nanoid } from 'nanoid/non-secure';
-import { useSelector } from 'react-redux';
+//import { useEffect } from 'react';
+
+//import { useSelector } from 'react-redux';
 import { Box } from './Box/Box';
 import { H1, H2 } from './Title/Title';
 import { ContactList } from './ContactList/ContactList';
 import { Phonebook } from './Phonebook/Phonebook';
 import { Filter } from './Filter/Filter';
-const LS_KEY = 'contacts';
+//const LS_KEY = 'contacts';
 
 export const App = () => {
-  const contacts = useSelector(state => state.contacts);
+  //const contacts = useSelector(state => state.contacts);
 
-  useEffect(() => {
-    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
-  }, [contacts]);
+  //useEffect(() => {
+  //  localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+  //}, [contacts]);
 
   return (
     <Box as="main" px={5}>
