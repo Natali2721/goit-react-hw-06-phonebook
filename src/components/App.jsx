cc1854c8a5e12a8b6de0364d96743f5f85a275be
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-12-09 16:29:32 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,79 +1,28 @@
-import { useEffect, useState } from 'react';
-import { nanoid } from 'nanoid/non-secure';
+import { useEffect } from 'react';
+//import { nanoid } from 'nanoid/non-secure';
+import { useSelector } from 'react-redux';
 import { Box } from './Box/Box';
 import { H1, H2 } from './Title/Title';
 import { ContactList } from './ContactList/ContactList';
 import { Phonebook } from './Phonebook/Phonebook';
 import { Filter } from './Filter/Filter';
-import { P } from './Style/Element.styled';
 const LS_KEY = 'contacts';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
-  });
-  const [filter, setFilter] = useState('');
+  const contacts = useSelector(state => state.contacts);
 
   useEffect(() => {
     localStorage.setItem(LS_KEY, JSON.stringify(contacts));
   }, [contacts]);
 
-  const formSubmit = ({ name, number }) => {
-    const id = nanoid();
-    const isExist = contacts.find(contact => contact.name === name);
-    console.log(isExist);
-    if (isExist) {
-      alert(`${name} is already in contacts`);
-      return;
-    }
-    setContacts(contacts => [
-      ...contacts,
-      {
-        name: name,
-        number: number,
-        id: id,
-      },
-    ]);
-  };
-
-  const handleFilterChange = e => {
-    const { value } = e.currentTarget;
-    setFilter(value);
-  };
-
-  const onClickDelete = id => {
-    setContacts(contacts.filter(contact => contact.id !== id));
-    //reset();
-  };
-  //const reset = () => {
-  //   setFilter('');
-  //};
-
-  const filterValueNormolize = filter.toLowerCase();
-  const contactsToShow = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filterValueNormolize)
-  );
-
   return (
     <Box as="main" px={5}>
       <H1 title="Phonebook" />
-      <Phonebook onSubmit={formSubmit} />
-      {contacts.length > 0 ? (
-        <>
-          <H2 title="Contacts" />
-          <Filter
-            title="Find contact by name"
-            onChange={handleFilterChange}
-            value={filter}
-          />
-          <ContactList
-            contacts={contactsToShow}
-            onClickDelete={onClickDelete}
-          />
-        </>
-      ) : (
-        <P>Phonebook is empty</P>
-      )}
+      <Phonebook />
+
+      <H2 title="Contacts" />
+      <Filter title="Find contact by name" />
+      <ContactList />
     </Box>
   );
 };
