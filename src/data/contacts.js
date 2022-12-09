c6ac9824de3d087793ffc67b65374981dc0943d0
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-12-09 16:52:49 +0200

    Create contacts.js

---
+++ src/data/contacts.js
@@ -0,0 +1,8 @@
+import { nanoid } from 'nanoid';
+
+export const INITIAL_CONTACTS = [
+  { id: nanoid(7), name: 'Ark', number: '655-65-65' },
+  { id: nanoid(7), name: 'Alex', number: '277-77-77' },
+  { id: nanoid(7), name: 'Polly', number: '433-43-43' },
+  { id: nanoid(7), name: 'Rosie', number: '122-12-12' },
+];
